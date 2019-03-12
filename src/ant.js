68b7952d452ae670118d92b2b39a6e97945bb42a
Tareq Hasan <tareq1988@gmail.com>
2019-03-12 23:19:39 +0600

    Import only used components

---
+++ src/ant.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import { Layout, Menu, Dropdown, Popover, Badge, Switch, Button, Icon } from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.use(Layout)
+Vue.use(Menu)
+Vue.use(Dropdown)
+Vue.use(Popover)
+Vue.use(Badge)
+Vue.use(Switch)
+Vue.use(Button)
+Vue.use(Icon)
--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd  from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
-Vue.use(Antd)
+import './ant'
 
 Vue.config.productionTip = false
 
